84de9818e6f3d9b69c5551e8ce058102c8b91494
ExpandingMan <savastio@gmail.com>
2017-05-14 17:27:48 -0400

    Started making opcode boilerplate stuff.

--- src/boilerplate.jl
+++ src/boilerplate.jl
@@ -1,4 +1,141 @@
 #===================================================================================================
 Here we keep some macros for doing boilerplate code generation...
 ===================================================================================================#
+# TODO deal with page crossings!!!
+
+opfuncname(opcode::UInt8) = Symbol(string("op", hexstring(opcode), "!"))
+
+
+opdict(opcode::UInt8, nbytes::Int) = :(OPCODES[$opcode] = ($nbytes, $(opfuncname(opcode))))
+
+
+function opdef_Immediate(opname::Symbol, opcode::UInt8, nbytes::Int, ncycles::Int)
+    fname = opfuncname(opcode)
+    quote
+        $fname(cs::Chipset, bytes::AbstractVector{UInt8}) = ($opname(cs.cpu, bytes2arg(bytes)); $nbytes)
+    end
+end
+
+function opdef_ZeroPage(opname::Symbol, opcode::UInt8, nbytes::Int, ncycles::Int)
+    fname = opfuncname(opcode)
+    quote
+        function $fname(cs::Chipset, bytes::AbstractVector{UInt8})
+            $opname(cs.cpu, cs.ram, Π8(bytes))
+            $ncycles
+        end
+    end
+end
+
+function opdef_ZeroPageX(opname::Symbol, opcode::UInt8, nbytes::Int, ncycles::Int)
+    fname = opfuncname(opcode)
+    quote
+        function $fname(cs::Chipset, bytes::AbstractVector{UInt8})
+            $opname(cs.cpu, cs.ram, DirectX, Π8(bytes))
+            $ncycles
+        end
+    end
+end
+
+function opdef_ZeroPageY(opname::Symbol, opcode::UInt8, nbytes::Int, ncycles::Int)
+    fname = opfuncname(opcode)
+    quote
+        function $fname(cs::Chipset, bytes::AbstractVector{UInt8})
+            $opname(cs.cpu, cs.ram, DirectY, Π8(bytes))
+            $ncycles
+        end
+    end
+end
+
+function opdef_Absolute(opname::Symbol, opcode::UInt8, nbytes::Int, ncycles::Int)
+    fname = opfuncname(opcode)
+    quote
+        function $fname(cs::Chipset, bytes::AbstractVector{UInt8})
+            $opname(cs.cpu, cs.ram, Π16(bytes))
+            $ncycles
+        end
+    end
+end
+
+function opdef_AbsoluteX(opname::Symbol, opcode::UInt8, nbytes::Int, ncycles::Int)
+    fname = opfuncname(opcode)
+    quote
+        function $fname(cs::Chipset, bytes::AbstractVector{UInt8})
+            $opname(cs.cpu, cs.ram, DirectX, Π16(bytes))
+            $ncycles
+        end
+    end
+end
+
+function opdef_AbsoluteY(opname::Symbol, opcode::UInt8, nbytes::Int, ncycles::Int)
+    fname = opfuncname(opcode)
+    quote
+        function $fname(cs::Chipset, bytes::AbstractVector{UInt8})
+            $opname(cs.cpu, cs.ram, DirectY, Π16(bytes))
+            $ncycles
+        end
+    end
+end
+
+function opdef_IndirectX(opname::Symbol, opcode::UInt8, nbytes::Int, ncycles::Int)
+    fname = opfuncname(opcode)
+    quote
+        function $fname(cs::Chipset, bytes::AbstractVector{UInt8})
+            $opname(cs.cpu, cs.ram, IndirectX, Π8(bytes))
+            $ncycles
+        end
+    end
+end
+
+function opdef_IndirectY(opname::Symbol, opcode::UInt8, nbytes::Int, ncycles::Int)
+    fname = opfuncname(opcode)
+    quote
+        function $fname(cs::Chipset, bytes::AbstractVector{UInt8})
+            $opname(cs.cpu, cs.ram, IndirectY, Π8(bytes))
+            $ncycles
+        end
+    end
+end
+
+
+
+
+
+function opdef(mode::Symbol, opname::Symbol, opcode::UInt8, nbytes::Int, ncycles::Int)
+    if mode == :Implicit
+        return opdef_Implicit(opname, opcode, nbytes, ncycles)
+    elseif mode == :Accumulator
+        return opdef_Accumulator(opname, opcode, nbytes, ncycles)
+    elseif mode == :Immediate
+        return opdef_Immediate(opname, opcode, nbytes, ncycles)
+    elseif mode == :ZeroPage
+        return opdef_ZeroPage(opname, opcode, nbytes, ncycles)
+    elseif mode == :ZeroPageX
+        return opdef_ZeroPageX(opname, opcode, nbytes, ncycles)
+    elseif mode == :ZeroPageY
+        return opdef_ZeroPageY(opname, opcode, nbytes, ncycles)
+    elseif mode == :Absolute
+        return opdef_Absolute(opname, opcode, nbytes, ncycles)
+    elseif mode == :AbsoluteX
+        return opdef_AbsoluteX(opname, opcode, nbytes, ncycles)
+    elseif mode == :AbsoluteY
+        return opdef_AbsoluteY(opname, opcode, nbytes, ncycles)
+    elseif mode == :IndirectX
+        return opdef_IndirectX(opname, opcode, nbytes, ncycles)
+    elseif mode == :IndirectY
+        return opdef_IndirectY(opname, opcode, nbytes, ncycles)
+    elseif mode == :Relative
+        return opdef_Relative(opname, opcode, nbytes, ncycles)
+    else
+        throw(ArgumentError("Invalid oparation mode $mode."))
+    end
+end
+
+
+
+
+
+
+macro opdef(opname::Symbol, defblock::Expr)
+
+end
 
--- src/opcodes.jl
+++ src/opcodes.jl
@@ -1,5 +1,48 @@
-#===================================================================================================
-TODO write these in such a way that it'll still be possible to generate Julia code from
-instructions.
-===================================================================================================#
 
+# this doesn't need to be exact, used for setting the dict size
+const N_OPCODES = 256
+
+
+instruction(c::CPU, m::Memory) = deref(Π(c.PC), m)
+instruction(cs::Chipset) = instruction(cs.cpu, cs.ram)
+
+
+# this doesn't do the sleeping
+function compute!(cs::Chipset)
+    nbytes, ncycles = op!(cs, Π(c.PC))
+    cs.cpu.PC += nbytes
+    ncycles
+end
+
+
+function op!(cs::Chipset, ptr::Π)
+    opcode = deref(ptr, cs.ram)
+    exe!, nbytes = OPCODES[opcode]
+    ncycles = exe!(cs, deref_opargs(cs, ptr, nbytes))
+    nbyes, ncycles
+end
+
+
+
+#===================================================================================================
+    <opcodes>
+===================================================================================================#
+const OPCODES = Dict{Int,Tuple{Function, Int}}();  sizehint!(OPCODES, N_OPCODES)
+
+
+# TODO this is a mock-up of what this should ultimately look like
+@opdef lda! begin
+    Immediate, 0x69, 2, 2
+    ZeroPage,  0x65, 2, 3
+    ZeroPageX, 0x75, 2, 4
+    Absolute,  0x6d, 3, 4
+    AbsoluteX, 0x7d, 3, 4  # should automatically take care of page crossings
+    AbsoluteY, 0x79, 3, 4
+    IndirectX, 0x61, 2, 6
+    IndirectY, 0x71, 2, 5
+end
+
+
+#===================================================================================================
+    </opcodes>
+===================================================================================================#
